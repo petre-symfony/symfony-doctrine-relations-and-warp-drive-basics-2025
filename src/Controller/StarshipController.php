28c705088b0f3c54da2e50fe87ecce3c6f773644
petrero <petrero@gmail.com>
2025-09-01 11:50:10 +0300

    6.1 Fetching a Relation's Data
     Navigate to the homepage and click on any of the starships with an 'In Progress' status.
    
     Hey! We're already listing the parts... sort of... these are all hard-coded!
    
     Now, how do we get the parts that are related to this ship?
    
     Open the controller for this page: src/Controller/StarshipController.php
    
     Querying for Related Parts Like any Other Property
     - To query for parts, we would typically autowire the StarshipPartRepository. Start the same way here with a StarshipPartRepository $partRepository argument
    
     Next, set $parts, to $partRepository->findBy():
    
     This is pretty standard stuff: if you want to query where some property equals a value, use findBy() and pass the property name and the value. When it comes to relationships, it's the same darn thing!
    
      $parts = $partRepository->findBy(['starship' => $ship]).
    
     And no, we're not doing Starship ID or anything of the sort. Keep IDs out of this! Instead, pass the Starship object itself. You could pass the id if you're feeling lazy, but in the spirit of Doctrine, relationships, and thinking about objects, passing the entire Starship object is the way to go.
    
     Let's debug and see what we've got: dd($parts)
    
     Refresh, and voila! An array of 10 StarshipPart objects, all related to this Starship. Pretty awesome, right? If you think so, hold onto your pants.

--- src/Controller/StarshipController.php
+++ src/Controller/StarshipController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Starship;
+use App\Repository\StarshipPartRepository;
 use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,7 +14,10 @@ class StarshipController extends AbstractController {
 	public function show(
 		#[MapEntity(mapping: ['slug' => 'slug'])]
 		Starship $ship,
+		StarshipPartRepository $partRepository
 	): Response {
+		$parts = $partRepository->findBy(['starship' => $ship]);
+		dd($parts);
 		return $this->render('starship/show.html.twig', [
 			'ship' => $ship,
 		]);
