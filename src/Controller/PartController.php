65ad6df19926b6187dee3292ea6b7f60b905321f
petrero <petrero@gmail.com>
2025-09-02 11:38:43 +0300

    11.2 Change the URL to /parts, and rename it to app_part_index
     Copy the route name so we can link to it... and open up base.html.twig.
    
     Linking to the Parts Page
     - Remember that "About" link that's sitting there doing nothing? Commandeer that and turn it into a "Parts" link. Set the href to {{ path('app_part_index') }}
    
     Head to the homepage, click our newly minted link, and... well, it's not the prettiest sight, but it works!

--- src/Controller/PartController.php
+++ src/Controller/PartController.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class PartController extends AbstractController {
-	#[Route('/part', name: 'app_part')]
+	#[Route('/part', name: 'app_part_index')]
 	public function index(): Response {
 		return $this->render('part/index.html.twig', [
 			'controller_name' => 'PartController',
