1251c94dc9e5d0332c0a99a2cd5a9ea6d6862e4d
petrero <petrero@gmail.com>
2025-09-02 11:44:26 +0300

    11.4 Adding Some Substance: Looping Over Parts  - To loop over the parts, in PartController, we need to query for all the parts.
     Add a StarshipPartRepository argument to autowire that in. Call it whatever you want, like $leeroyJenkins or... $repository. To get all the parts — it's simple: $parts = repository->findAll()

--- src/Controller/PartController.php
+++ src/Controller/PartController.php
@@ -2,15 +2,18 @@
 
 namespace App\Controller;
 
+use App\Repository\StarshipPartRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class PartController extends AbstractController {
 	#[Route('/part', name: 'app_part_index')]
-	public function index(): Response {
+	public function index(StarshipPartRepository $repository): Response {
+		$parts = $repository->findAll();
+
 		return $this->render('part/index.html.twig', [
-			'controller_name' => 'PartController',
+			'parts' => $parts
 		]);
 	}
 }
