f23b780cb3276c91c8fa7a813116f6b53b8cf6e7
petrero <petrero@gmail.com>
2025-09-03 12:05:03 +0300

    15.1 Many-To-Many Relationship  Alrighty, we've got a Starship entity and a Droid entity set up and ready to mingle. How do we get these two entities to connect?
     Picture it this way: Each Starship is going to need a crew of Droids to keep things running smoothly... and for the occasional comic relief. Each Droid, in turn, should be able to serve on many Starships. Forget about the database and just focus on the objects. Our Starship entity needs a droids property that holds a collection of all the Droids assigned to it.
    
     Cool! Head back to your terminal and run:
    
      symfony console make:entity
     Update Starship and add a droids property. Use "relation" to get into the handy wizard. This time, we need a ManyToMany relationship:
    
      "Each Starship can have many Droids, and each Droid can serve on many Starships. That sounds perfect! "
    
     Next, it asks us if we want to map the inverse side of the relationship. This is asking if we want to give our Droids the ability to list all the Starships they're connected to: $droid->getShips(). That sounds useful. So let's say "yes". For the new field name inside Droid, starships will do just fine.
    
     Notice it's updated both Starship and Droid. Take a peek at the changes in each.
    
     The 'ManyToMany' Magic
     - In Starship, we now have a new droids property, which is a ManyToMany. It also initialized droids to the ArrayCollection and added getDroids(), addDroid(), and removeDroid() methods
    
     If you're thinking this looks a lot like a OneToMany relationship, ding, ding! Order yourself a pizza! Because it totally is!
    
     Over in Droid, it's a similar story. We have a starships property, which is a ManyToMany, and it's initialized in the constructor. Then we have the same getStarships(), addStarship(), and removeStarship()

--- src/Entity/Droid.php
+++ src/Entity/Droid.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\DroidRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: DroidRepository::class)]
@@ -17,6 +19,16 @@ class Droid {
 
 	#[ORM\Column(length: 255)]
 	private ?string $primaryFunction = null;
+
+	/**
+	 * @var Collection<int, Starship>
+	 */
+	#[ORM\ManyToMany(targetEntity: Starship::class, mappedBy: 'droids')]
+	private Collection $starships;
+
+	public function __construct() {
+		$this->starships = new ArrayCollection();
+	}
 
 	public function getId(): ?int {
 		return $this->id;
@@ -38,6 +50,30 @@ class Droid {
 
 	public function setPrimaryFunction(string $primaryFunction): static {
 		$this->primaryFunction = $primaryFunction;
+
+		return $this;
+	}
+
+	/**
+	 * @return Collection<int, Starship>
+	 */
+	public function getStarships(): Collection {
+		return $this->starships;
+	}
+
+	public function addStarship(Starship $ship): static {
+		if (!$this->starships->contains($ship)) {
+			$this->starships->add($ship);
+			$ship->addDroid($this);
+		}
+
+		return $this;
+	}
+
+	public function removeStarship(Starship $ship): static {
+		if ($this->starships->removeElement($ship)) {
+			$ship->removeDroid($this);
+		}
 
 		return $this;
 	}
